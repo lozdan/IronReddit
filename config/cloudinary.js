3568e058c0867c041f0be47b99b1381731f3bc04
Daniel Lozano <lozanodaniel02@gmail.com>
2018-05-11 16:26:06 -0400

    add secrets

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,9 +3,9 @@ const cloudinaryStorage = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 cloudinary.config({
-    cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
-    api_secret: process.env.api_secret
+    cloud_name: 'ddibftjux',
+    api_key: '615143174846895',
+    api_secret: '-h0PLS8txRaVGzRs3ICORuQwLKY'
 });
 
 var myStorage = cloudinaryStorage({
